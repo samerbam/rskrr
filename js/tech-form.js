dadcb5f8a52ae869aa2884c942cadfe754dcb6f6
Sam <r.sam.ryan@gmail.com>
2020-08-11 13:05:09 -0400

    Seperated form js to tech-form.js, Refactored phone number input box code.

--- js/tech-form.js
+++ js/tech-form.js
@@ -1,93 +1,197 @@
-//Example Request: {"name": "Sam Ryan","device": "iPhone","issue": "Will Not Turn On.","contact_method": "Email","contact_details": "example@example.com"}
-
-
-// Example POST method implementation:
-async function postData(url = '', data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-//    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'plain/text'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-//	body: data
-  })//.then(function (text) {
-	 // console.log(text)
-  //});
-  return response // parses JSON response into native JavaScript objects
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction(id) {
+	document.getElementById("myDropdown_"+id).classList.toggle("show");
+//  console.log('test')
 }
 
-//postData('https://example.com/answer', { answer: 42 })
-//  .then(data => {
-//    console.log(data); // JSON data parsed by `data.json()` call
-//  });
-
-async function submit_form() {
-	const data = {"name": "Sam Ryan","device": "iPhone","issue": "Will Not Turn On.","contact_method": "Email","contact_details": "example@example.com"}
-	
-//	postData('https://tech-form.imsam.ca', data)
-//	.then(function (text) {
-//		console.log(text)
-//	});
-	
-	
-	
-	
-	
-//  .then(data => {
-//	let v = data.text()
-//    console.log(data); // JSON data parsed by `data.json()` call
-//  });
-	
-//	const data = {
-//        message: 'We send a message to the backend with fetch()'
-//    };
-//	const endpoint = 'https://rskrr.duckdns.org:8123/api/webhook/ApbWZRjsh75sj7kZFLsMNewxVgVWNKCf';
-//
-//fetch(endpoint, {
-//    method: 'POST',
-//    body: JSON.stringify(data)
-//})
-//.then((resp) => resp.json())
-//.then(function(response) {
-//    console.info('fetch()', response);
-//    return response;
-//});
-	
-//	var xhr = new XMLHttpRequest();
-//	
-//	xhr.onreadystatechange = function () {
-//    if (this.readyState != 4) return;
-//
-//    if (this.status == 200) {
-//        var data = JSON.parse(this.responseText);
-//		console.log(data)
-//
-//        // we get the returned data
-//    }
-//
-//    // end of state change: it can be after some time (async)
-//};
-//	xhr.open("POST", "https://tech-form.imsam.ca", true);
-//	xhr.setRequestHeader('Content-Type', 'application/json');
-//	xhr.send(JSON.stringify(data));
-//	console.log(xhr.responseText);
-//	res = await fetch("https://tech-form.imsam.ca", {
-//	method: "POST",
-//		mode: 'cors',
-//	body: JSON.stringify(data)
-////	headers: {
-////			'Content-Type': 'application/json'
-////	}
-//	}).then()
-//	console.log(res)
-//	}).then(res => {
-////		console.log("Request complete! response:", res);
-//	});
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
 }
+
+function changeSelection(element) {
+//  document.getElementsByClassName('dropbtn').forEach(ele => {
+	[].forEach.call(document.getElementsByClassName('dropbtn'), function (ele) {
+		if (ele.dataset.id == element.parentElement.dataset.id) {
+			ele.innerHTML = element.innerHTML;
+			ele.parentElement.children[0].value = element.innerHTML;
+//      console.log(ele.parentElement.children)
+		}
+	});
+}
+
+
+const tx = document.getElementsByTagName('textarea');
+for (let i = 0; i < tx.length; i++) {
+	tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px;overflow-y:hidden;');
+	tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+	this.style.height = 'auto';
+	this.style.height = (this.scrollHeight) + 'px';
+}
+
+String.prototype.replaceAt = function(index, replacement) {
+		return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+}
+
+// function indexOfLastDigit(input) {
+// 	let numsOnly = input.replace(/\D/g, "")
+// 	return input.lastIndexOf(numsOnly.slice(-1))
+// }
+
+// function phone_mask(event) {
+
+// 	const keyPressed = event.key
+// 	const el = document.getElementById("phoneNumber")
+// 	var placeholder = el.placeholder
+// 	const digits = el.value.replace(/[^0-9]/g,"").length
+
+// 	if (el.value == "") {
+// 		el.value = placeholder
+// 	}
+// 	if (digits == 0) {
+// 		el.value = placeholder
+// 	}
+
+// 	const nextLoc = el.value.indexOf("_")
+
+
+
+// 	if ((event.charCode >= 48 && event.charCode <= 57) || event.keyCode == 9 || event.keyCode == 10 || event.keyCode == 13 || event.keyCode == 116 || event.keyCode == 46 || (event.keyCode <= 40 && event.keyCode >= 37)) {
+// 		if (nextLoc != -1) {
+// 			el.value = el.value.replaceAt(nextLoc, keyPressed)
+// 			el.setSelectionRange(nextLoc+1,nextLoc+1)
+// 		}
+// 	}
+
+// 	/*
+// 	 * charCode [48,57]   Numbers 0 to 9
+// 	 * keyCode 46       "delete"
+// 	 * keyCode 9        "tab"
+// 	 * keyCode 13       "enter"
+// 	 * keyCode 116      "F5"
+// 	 * keyCode 8        "backscape"
+// 	 * keyCode 37,38,39,40  Arrows
+// 	 * keyCode 10     (LF)
+// 	 */
+// 	// return (event.charCode >= 48 && event.charCode <= 57) || event.keyCode == 9 || event.keyCode == 10 || event.keyCode == 13 || event.keyCode == 8 || event.keyCode == 116 || event.keyCode == 46 || (event.keyCode <= 40 && event.keyCode >= 37) ? true : false
+// 	event.preventDefault()
+// 	return false
+// }
+
+// function onDelete(event) {
+// 		const el = el
+
+// 	if (event.keyCode == 8) {
+// 		let d = indexOfLastDigit(el.value)
+// 		if (d>1) {
+// 			el.value = el.value.replaceAt(d, "_")
+// 			el.setSelectionRange(d,d)
+// 		}
+// 		event.preventDefault()
+// 	}
+// }
+
+// function tabFocus(e) {
+// 	var el = el
+// 	if ((e.keyCode ? e.keyCode : e.which)) {
+// 		if (el.value == "") {
+// 			el.value = el.placeholder
+// 		}
+// 		el.setSelectionRange(i, i)
+// 	}
+// }
+
+// function emptyFill() {
+//   var el = el
+//   var i = el.value.indexOf("_") == -1 ? el.value.length : el.value.indexOf("_")
+//   el.setSelectionRange(i, i)
+//   if (el.value == "") {
+//     el.value = el.placeholder
+//   }
+// }
+
+// el.onkeypress = phone_mask;
+// el.onkeydown = onDelete;
+// el.onkeyup = tabFocus;
+// el.onclick = emptyFill;
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	var el = document.getElementById("phoneNumber")
+
+	const setSelLoc = () => {
+		var i = el.value.indexOf("_") == -1 ? el.value.length : el.value.indexOf("_")
+		el.setSelectionRange(i, i)
+	},
+	indexOfLastDigit = (input) => {
+		let numsOnly = input.replace(/\D/g, "")
+		return input.lastIndexOf(numsOnly.slice(-1))
+	},
+	emptyFill = () => {
+		setSelLoc()
+		if (el.value == "") {
+			el.value = el.placeholder
+		}
+	},
+	tabFocus = (e) => {
+		if ((e.keyCode ? e.keyCode : e.which)) {
+			if (el.value == "") {
+				el.value = el.placeholder
+			}
+			setSelLoc()
+		}
+	},
+	onDelete = (event) => {
+		if (event.keyCode == 8) {
+			let d = indexOfLastDigit(el.value)
+			if (d>1) {
+				el.value = el.value.replaceAt(d, "_")
+				el.setSelectionRange(d,d)
+			}
+			event.preventDefault()
+		}
+	},
+	phone_mask = (event) => {
+		const keyPressed = event.key
+		const nextLoc = el.value.indexOf("_")
+		if ((event.charCode >= 48 && event.charCode <= 57) || event.keyCode == 9 || event.keyCode == 10 || event.keyCode == 13 || event.keyCode == 116 || event.keyCode == 46 || (event.keyCode <= 40 && event.keyCode >= 37)) {
+			if (nextLoc != -1) {
+				el.value = el.value.replaceAt(nextLoc, keyPressed)
+				el.setSelectionRange(nextLoc+1,nextLoc+1)
+			}
+		}
+
+		/*
+		 * charCode [48,57]   Numbers 0 to 9
+		 * keyCode 46       "delete"
+		 * keyCode 9        "tab"
+		 * keyCode 13       "enter"
+		 * keyCode 116      "F5"
+		 * keyCode 8        "backscape"
+		 * keyCode 37,38,39,40  Arrows
+		 * keyCode 10     (LF)
+		 */
+		// return (event.charCode >= 48 && event.charCode <= 57) || event.keyCode == 9 || event.keyCode == 10 || event.keyCode == 13 || event.keyCode == 8 || event.keyCode == 116 || event.keyCode == 46 || (event.keyCode <= 40 && event.keyCode >= 37) ? true : false
+		event.preventDefault()
+		return false
+	};
+	
+	el.onkeypress = phone_mask;
+	el.onkeydown = onDelete;
+	el.onkeyup = tabFocus;
+	el.onclick = emptyFill;
+})
---
+++ js/utils.js
@@ -0,0 +1,22 @@
+function checkBrowser() {
+	var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+	var isFirefox = typeof InstallTrigger !== 'undefined';
+	var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+	var isIE = /*@cc_on!@*/false || !!document.documentMode;
+	var isEdge = !isIE && !!window.StyleMedia;
+	var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+	var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
+	var isBlink = (isChrome || isOpera) && !!window.CSS;
+	
+	var browserList = {
+		"Opera": isOpera,
+		"Firefox": isFirefox,
+		"Safari": isSafari,
+		"IE": isIE,
+		"Edge": isEdge,
+		"Chrome": isChrome,
+		"EdgeChromium": isEdgeChromium,
+		"Blink": isBlink
+	}
+	
+}
