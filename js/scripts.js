c9f0069549c2dc40ea636505e00a4e3fbf33e97f
Sam <r.sam.ryan@gmail.com>
2020-09-28 15:54:01 -0400

    Added pricing to pricing page.

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,31 @@
  * keyCode 37,38,39,40	Arrows
  * keyCode 10			(LF)
  */
+
+var stripe = Stripe('pk_test_51H7ps1KmV0hnkyd1uoNY36oEjfDL4v88kW2kzSJeWuYcKdTluUxfSmvQFKtBN1dxvsayYaIg40GHXoC2m7IbbWeu00nwcuFZTb');
+
+function cTest() {
+stripe.redirectToCheckout({
+  lineItems: [
+  {
+    price: 'price_1HUfbaKmV0hnkyd1NFDdDYwV', // Replace with the ID of your price
+    quantity: 1,
+  },
+  {
+    price: 'price_1HW6EFKmV0hnkyd1ybbgRQvu',
+    quantity: 1,
+  },
+  {
+    price: 'price_1HUf4kKmV0hnkyd1TNssPatZ',
+    quantity: 1,
+  }
+  ],
+  mode: 'payment',
+  successUrl: 'https://imsam.ca',
+  cancelUrl: 'https://imsam.ca',
+}).then(function (result) {
+  // If `redirectToCheckout` fails due to a browser or network
+  // error, display the localized error message to your customer
+  // using `result.error.message`.
+});
+}
